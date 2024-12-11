e2ce8fb78af4af5cba9637bc9d82dddbd37893ae
julia85310 <juliarueda85310@gmail.com>
2024-12-11 10:38:37 +0100

    upd ejercicio 6

--- src/app/Ejercicio6.js
+++ src/app/Ejercicio6.js
@@ -2,23 +2,24 @@ import {useState, useEffect} from 'react';
 
 export default function Ejercicio6(){
   const [posts, setPosts] = useState([]); 
+  const [cargando, setCargando] = useState(true);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => { //then captura la promesa de fetch cuando pasa de pendiente a cumplida
-        if (!response.ok) {
-          throw new Error('Error en la solicitud'); 
-        }
-        return response.json(); 
-      })
+      .then((response) => response.json())
       .then((data) => {
         setPosts(data); 
+        setCargando(false);
       })
       .catch((error) => {
         console.error('Error al obtener los posts:', error);
       });
   }, []); 
 
+  if (cargando) {
+    return <h2>Cargando usuarios...</h2>;
+  }
+  
   return <ul>
         {posts.map((post) => (
           <li key={post.id}>
